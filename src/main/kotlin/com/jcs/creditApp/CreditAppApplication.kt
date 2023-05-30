4b1422a67548089a37e66f13a40a8067c75ee867
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2023-05-30 18:24:32 -0300

    Created project

---
+++ src/main/kotlin/com/jcs/creditApp/CreditAppApplication.kt
@@ -0,0 +1,11 @@
+package com.jcs.creditApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/jcs/creditApp/CreditAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jcs.creditApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
